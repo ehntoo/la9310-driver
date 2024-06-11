b59e7e543426c7e9d796423485ab115095b145cb
Mitchell Johnson <ehntoo@ehntoo.org>
2024-06-10 22:27:07 -0400

    Clean up dead code

--- kernel_driver/la9310rfnm/rfnm_lime_m.c
+++ kernel_driver/la9310rfnm/rfnm_lime_m.c
@@ -5,8 +5,6 @@
 #include <linux/dma-mapping.h>
 #include <linux/dma-mapping.h>
 #include <la9310_base.h>
-//#include "rfnm.h"
-//#include "rfnm_callback.h"
 #include <asm/cacheflush.h>
 
 #include <linux/dma-direct.h>
@@ -40,9 +38,6 @@
 #include <linux/platform_device.h>
 #include <linux/spi/spi.h>
 
-// void kernel_neon_begin(void);
-// void kernel_neon_end(void);
-
 #include "limesuiteng/embedded/lms7002m/lms7002m.h"
 
 #include "rfnm_lime0_regs.h"
@@ -55,65 +50,6 @@
 #include "rfnm_fe_generic.h"
 #include "rfnm_fe_lime0.h"
 
-#include "dtoa.h"
-
-
-//void *lms_spi_handle;
-
-
-/*struct rfnm_lms_spi {
-	struct spi_controller *ctlr;
-	void __iomem *base;
-};*/
-
-// static int lms_spi16_transact(const uint32_t* mosi, uint32_t* miso, uint32_t count, void* userData) {
-//     struct spi_device *spi = (struct spi_device *)userData;
-// 	struct spi_transfer xfer;
-// 	uint32_t *txbuf = kmalloc(4, GFP_KERNEL);
-// 	uint32_t *rxbuf = kmalloc(4, GFP_KERNEL);
-// 	size_t i;
-// 	int ret;
-
-// 	kernel_neon_end();
-
-// 	for (i=0; i < count; i++) {
-// 		memset(&xfer, 0, sizeof(xfer));
-// 		*txbuf = mosi[i];
-// 		// txbuf = ((mosi[i] & 0xFF) << 24) |
-// 		// 	((mosi[i] & 0xFF00) << 8) |
-// 		// 	((mosi[i] & 0xFF0000) >> 8) |
-// 		// 	((mosi[i] & 0xFF000000) >> 24);
-// 		xfer.tx_buf = txbuf;
-// 		xfer.len = 4;
-// 		if (!(*txbuf & (1 << 31)) && miso)
-// 			xfer.rx_buf = rxbuf;
-
-// 		ret = spi_sync_transfer(spi, &xfer, 1);
-
-// 		if (ret < 0) {
-// 			printk("spi_sync_transfer failed: %d\n", ret);
-// 			//return ERR_SI_CORE_MAX_CNTWRTE;
-// 		// } else {
-// 		// 	printk("spi_sync_transfer wrote: 0x%08x, read 0x%08x\n", txbuf, rxbuf);
-// 		}
-
-// 		// if this is a read, signified by the most significant bit being cleared,
-// 		// we need the data in the received buffer. If the miso output buffer is
-// 		// not NULL, stick the data in there.
-// 		if (!(*txbuf & (1 << 31)) && miso) {
-// 			miso[i] = *rxbuf;
-// 			// miso[i] = ((rxbuf & 0xFF) << 24) |
-// 			// ((rxbuf & 0xFF00) << 8) |
-// 			// ((rxbuf & 0xFF0000) >> 8) |
-// 			// ((rxbuf & 0xFF000000) >> 24);
-// 		}
-// 	}
-// 	kfree(rxbuf);
-// 	kfree(txbuf);
-
-// 	kernel_neon_begin();
-// 	return 0;
-// }
 
 static int lms_spi16_transact(const uint32_t* mosi, uint32_t* miso, uint32_t count, void* userData) {
     struct spi_device *spi = (struct spi_device *)userData;
@@ -122,8 +58,6 @@ static int lms_spi16_transact(const uint32_t* mosi, uint32_t* miso, uint32_t cou
 	uint32_t rxbuf;
 	size_t i;
 	int ret;
-
-	// kernel_neon_end();
 
 	for (i=0; i < count; i++) {
 		memset(&xfer, 0, sizeof(xfer));
@@ -147,7 +81,6 @@ static int lms_spi16_transact(const uint32_t* mosi, uint32_t* miso, uint32_t cou
 		}
 	}
 
-	// kernel_neon_begin();
 	return 0;
 }
 
@@ -186,11 +119,6 @@ int parse_lime_iq_lpf(int mhz) {
 }
 
 void lime0_set_iq_tx_lpf_bandwidth(struct rfnm_dgb *dgb_dt, int txbw) {
-
-	/*struct rfnm_dgb *dgb_dt;
-	dgb_dt = spi_get_drvdata(spi);
-	LMS7002M_t *lms;
-    lms = dgb_dt->priv_drv;*/
 	struct lms7002m_context *lms;
     lms = dgb_dt->priv_drv;
 
@@ -292,7 +220,7 @@ void lime0_set_iq_tx_lpf_bandwidth(struct rfnm_dgb *dgb_dt, int txbw) {
 	// LMS7002M_regs_spi_write(lms, 0x010a);
 
 	// lms->regs->reg_0x010b_value = 1;
-	// LMS7002M_regs_spi_write(lms, 0x010b);     
+	// LMS7002M_regs_spi_write(lms, 0x010b);
 
 	// if(txbw >= 50) {
 	// 	LMS7002M_tbb_set_path(lms, LMS_CHA, LMS7002M_TBB_HBF);
@@ -303,7 +231,6 @@ void lime0_set_iq_tx_lpf_bandwidth(struct rfnm_dgb *dgb_dt, int txbw) {
 	// }
 }
 
-
 void rfnm_lime_set_bias_t(struct rfnm_dgb *dgb_dt, enum rfnm_bias_tee bias_tee) {
 	if(bias_tee == RFNM_BIAS_TEE_ON) {
 		rfnm_fe_srb(dgb_dt, RFNM_LIME0_ANT_BIAS, 0);
@@ -313,7 +240,6 @@ void rfnm_lime_set_bias_t(struct rfnm_dgb *dgb_dt, enum rfnm_bias_tee bias_tee) 
 }
 
 void lime0_set_rx_gain(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
-
 	struct lms7002m_context *lms;
     lms = dgb_dt->priv_drv;
 	int dbm = rx_ch->gain;
@@ -336,9 +262,6 @@ void lime0_set_rx_gain(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
 		}
 	}
 
-	
-
-    //std::cout << "lms_gain_lna [0-30]" << endl;
     if(dbm >= 0) {
         if(dbm > 30) {
             dbm = 30;
@@ -347,11 +270,6 @@ void lime0_set_rx_gain(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
     } else {
         lms7002m_set_rfelna_db(lms, 0, LMS7002M_CHANNEL_AB);
 	}
-
-    //std::cout << "lms_gain_tia [0-12]" << endl;
-    //LMS7002M_rfe_set_tia(lms, LMS_CHAB, (float) gain);
-    //std::cout << "lms_gain_pga [0-32]" << endl;
-    //LMS7002M_rbb_set_pga(lms, LMS_CHAB, (float) gain);
 }
 
 void rfnm_tx_ch_get(struct rfnm_dgb *dgb_dt, struct rfnm_api_tx_ch * tx_ch) {
@@ -361,15 +279,11 @@ void rfnm_rx_ch_get(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
 	printk("inside rfnm_rx_ch_get\n");
 }
 
-
-
 int rfnm_tx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_tx_ch * tx_ch) {
-	// kernel_neon_begin();
 	rfnm_api_failcode ecode = RFNM_API_OK;
 	lime_Result ret;
     uint64_t actualRate = 0;
-	uint64_t freq = tx_ch->freq / (1000 * 1000);
-	// LMS7002M_t *lms;
+	uint64_t freq = tx_ch->freq / (MHZ_TO_HZ(1));
 	struct lms7002m_context *lms;
     lms = dgb_dt->priv_drv;
 
@@ -377,12 +291,8 @@ int rfnm_tx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_tx_ch * tx_ch) {
 
 	// need to enable those modules before other writes
 	lms7002m_enable_channel(lms, true, LMS7002M_CHANNEL_A, true);
-	// LMS7002M_sxx_enable(lms, LMS_TX, true);
-	// LMS7002M_tbb_enable(lms, LMS_CHA, true);
-	// LMS7002M_trf_enable(lms, LMS_CHA, true);
 
 	// this might fail because some tx paths are disabled when run in rx mode?
-	// ret = LMS7002M_set_lo_freq(lms, LMS_TX, LMS_REF_FREQ, tx_ch->freq, &actualRate);
 	ret = lms7002m_set_frequency_sx(lms, true, tx_ch->freq);
 	if(ret) {
 		printk("Tuning failed\n");
@@ -390,16 +300,13 @@ int rfnm_tx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_tx_ch * tx_ch) {
 		goto fail;
 	}
 	actualRate = lms7002m_get_frequency_sx(lms, true);
-	// printk("%d - Actual TX LO freq %s MHz\n", ret, dtoa1(actualRate/1e6));
 	printk("%d - Actual TX LO freq %llu Hz\n", ret, actualRate);
 
 	if(freq < 1500) {
 		lime0_tx_band(dgb_dt, RFNM_LIME0_TX_BAND_LOW);
-		// LMS7002M_trf_select_band(lms, LMS_CHA, 2);
 		lms7002m_set_band_trf(lms, 2);
 	} else {
 		lime0_tx_band(dgb_dt, RFNM_LIME0_TX_BAND_HIGH);
-		// LMS7002M_trf_select_band(lms, LMS_CHA, 1);
 		lms7002m_set_band_trf(lms, 1);
 	}
 
@@ -407,7 +314,7 @@ int rfnm_tx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_tx_ch * tx_ch) {
 	lime0_tx_lpf(dgb_dt, freq);
 
 	lime0_set_iq_tx_lpf_bandwidth(dgb_dt, parse_lime_iq_lpf(tx_ch->rfic_lpf_bw));
-	// lms7002m_set_tx_lpf(lms, tx_ch->rfic_lpf_bw * 1000000);
+	// lms7002m_set_tx_lpf(lms, MHZ_TO_HZ(tx_ch->rfic_lpf_bw));
 
 	if(tx_ch->path != RFNM_PATH_LOOPBACK) {
 		lime0_ant_tx(dgb_dt);
@@ -424,33 +331,28 @@ int rfnm_tx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_tx_ch * tx_ch) {
 	rfnm_fe_trigger_latches(dgb_dt);
 
 	if(tx_ch->enable == RFNM_CH_ON_TDD) {
-		memcpy(&dgb_dt->fe_tdd[RFNM_TX], &dgb_dt->fe, sizeof(struct fe_s));	
+		memcpy(&dgb_dt->fe_tdd[RFNM_TX], &dgb_dt->fe, sizeof(struct fe_s));
 		if(dgb_dt->rx_s[0]->enable == RFNM_CH_ON_TDD) {
 			rfnm_dgb_en_tdd(dgb_dt, tx_ch, dgb_dt->rx_s[0]);
 		}
 	}
 
-	memcpy(dgb_dt->tx_s[0], dgb_dt->tx_ch[0], sizeof(struct rfnm_api_tx_ch));	
-
-
-	// kernel_neon_end();
+	memcpy(dgb_dt->tx_s[0], dgb_dt->tx_ch[0], sizeof(struct rfnm_api_tx_ch));
 
 	return 0;
 
-fail: 
-	// kernel_neon_end();
+fail:
 	return -ecode;
 
 }
 
 int rfnm_rx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
-	// kernel_neon_begin();
 	lime_Result ret;
 	rfnm_api_failcode ecode = RFNM_API_OK;
     uint64_t actualRate = 0;
 	struct lms7002m_context *lms;
     lms = dgb_dt->priv_drv;
-	uint64_t freq = rx_ch->freq / (1000 * 1000);
+	uint64_t freq = rx_ch->freq / (MHZ_TO_HZ(1));
 
 	if(rx_ch->fm_notch == RFNM_FM_NOTCH_AUTO) {
 		lime0_fm_notch(dgb_dt, 1);
@@ -459,7 +361,7 @@ int rfnm_rx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
 	} else if(rx_ch->fm_notch == RFNM_FM_NOTCH_OFF) {
 		lime0_fm_notch(dgb_dt, 0);
 	}
-	
+
 
 	if(freq <= 2) {
 		lime0_filter_0_2(dgb_dt);
@@ -501,59 +403,36 @@ int rfnm_rx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
 
 	if(freq <= 60) {
 		lms7002m_set_path_rfe(lms, LMS7002M_PATH_RFE_LNAL);
-		// LMS7002M_rfe_set_path(lms, LMS_CHA, LMS7002M_RFE_LNAL);
 	} else if(freq <= 1400) {
-		// LMS7002M_rfe_set_path(lms, LMS_CHA, LMS7002M_RFE_LNAW);
 		lms7002m_set_path_rfe(lms, LMS7002M_PATH_RFE_LNAW);
 		rfnm_fe_srb(dgb_dt, RFNM_LIME0_LRIM, 0);
 	} else {
-		// LMS7002M_rfe_set_path(lms, LMS_CHA, LMS7002M_RFE_LNAH);
 		lms7002m_set_path_rfe(lms, LMS7002M_PATH_RFE_LNAH);
 		rfnm_fe_srb(dgb_dt, RFNM_LIME0_LRIM, 1);
 	}
-	
+
 	if(freq < 30) {
 		freq = 30;
 	}
-	// ret = LMS7002M_set_lo_freq(lms, LMS_RX, LMS_REF_FREQ, rx_ch->freq, &actualRate);
-	ret = lms7002m_set_frequency_sx(lms, false, rx_ch->freq);
-	actualRate = lms7002m_get_frequency_sx(lms, false);
 
+	ret = lms7002m_set_frequency_sx(lms, false, rx_ch->freq);
 	if(ret) {
-		printk("Tuning failed\n");
+		printk("Tuning failed!\n");
 		ecode = RFNM_API_TUNE_FAIL;
 		goto fail;
 	}
-	// printk("%d - Actual RX LO freq %s MHz\n", ret, dtoa1(actualRate/1e6));
+
+	actualRate = lms7002m_get_frequency_sx(lms, false);
 	printk("%d - Actual RX LO freq %llu Hz\n", ret, actualRate);
 
-	
-
-	// lime0_set_iq_rx_lpf_bandwidth(dgb_dt, parse_lime_iq_lpf(rx_ch->rfic_lpf_bw));
-	lms7002m_set_rx_lpf(lms, rx_ch->rfic_lpf_bw * 1000ll * 1000);
-
-
+	lms7002m_set_rx_lpf(lms, MHZ_TO_HZ(rx_ch->rfic_lpf_bw));
 
 	if(rx_ch->rfic_dc_q || rx_ch->rfic_dc_i) {
-
-		// LMS7002M_set_mac_ch(lms, LMS_CHA);
-		// lms7002m_set_active_channel(lms, LMS7002M_CHANNEL_A);
-
 		int16_t q = rx_ch->rfic_dc_q;
 		int16_t i = rx_ch->rfic_dc_i;
-                        
-		// if(i == 0 && q == 0) {
-		// 	lms->regs->reg_0x010d_en_dcoff_rxfe_rfe = 0;
-		// 	//printk("disabling lms dc offset\n");
-		// } else {
-		// 	lms->regs->reg_0x010d_en_dcoff_rxfe_rfe = 1;
-		// 	//printk("enabling lms dc offset\n");
-		// }
-
-		// LMS7002M_regs_spi_write(lms, 0x010d);
 
 		if(q > 63 || q < -63 || i > 63 || i < -63) {
-			printk("lms_set_dcoff out of range q [-63 63] i [-63 63]");
+			printk("lms_set_dcoff out of range q [-63 63] i [-63 63]\n");
 		}
 
 		if(q < 0) {
@@ -566,26 +445,9 @@ int rfnm_rx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
 			i |= (1 << 6);
 		}
 
-		//printk("%02x %02x \n", q, i);
-
-		// lms->regs->reg_0x010e_dcoffq_rfe = q & 0x7f;
-		// lms->regs->reg_0x010e_dcoffi_rfe = i & 0x7f;
-
-		// LMS7002M_regs_spi_write(lms, 0x010e);
-		// lms7002m_load_dc_reg_iq(lms, false, i, q);
-		// uint32_t write_dcoff_cmd = ((0x8000 | 0x010e) << 16) | ((i & 0x7f) << 7) | (q & 0x7f);
-		// spi_result = lms_spi16_transact(&write_dcoff_cmd, NULL, 1, spi);
 		lms7002m_set_dc_offset(lms, false, i, q);
 	}
 
-	
-
-
-	//LMS7002M_sxx_enable(lms, LMS_TX, false);
-	//LMS7002M_tbb_enable(lms, LMS_CHA, false);
-	//LMS7002M_trf_enable(lms, LMS_CHA, false);
-
-	
 	lime0_set_rx_gain(dgb_dt, rx_ch);
 
 	if(rx_ch->path != RFNM_PATH_LOOPBACK) {
@@ -604,21 +466,17 @@ int rfnm_rx_ch_set(struct rfnm_dgb *dgb_dt, struct rfnm_api_rx_ch * rx_ch) {
 
 
 	if(rx_ch->enable == RFNM_CH_ON_TDD) {
-		memcpy(&dgb_dt->fe_tdd[RFNM_RX], &dgb_dt->fe, sizeof(struct fe_s));	
+		memcpy(&dgb_dt->fe_tdd[RFNM_RX], &dgb_dt->fe, sizeof(struct fe_s));
 		if(dgb_dt->tx_s[0]->enable == RFNM_CH_ON_TDD) {
 			rfnm_dgb_en_tdd(dgb_dt, dgb_dt->tx_s[0], rx_ch);
 		}
 	}
 
-	memcpy(dgb_dt->rx_s[0], dgb_dt->rx_ch[0], sizeof(struct rfnm_api_rx_ch));	
-
-
-	// kernel_neon_end();
+	memcpy(dgb_dt->rx_s[0], dgb_dt->rx_ch[0], sizeof(struct rfnm_api_rx_ch));
 
 	return 0;
 
-fail: 
-	// kernel_neon_end();
+fail:
 	return -ecode;
 }
 
@@ -635,7 +493,6 @@ static void rfnm_lime_cgen_frequency_changed(void* handle) {
 
 static int rfnm_lime_probe(struct spi_device *spi)
 {
-	// kernel_neon_begin();
 	struct rfnm_bootconfig *cfg;
 	struct rfnm_eeprom_data *eeprom_data;
 	cfg = memremap(RFNM_BOOTCONFIG_PHYADDR, SZ_4M, MEMREMAP_WB);
@@ -646,9 +503,7 @@ static int rfnm_lime_probe(struct spi_device *spi)
 
 	if(	cfg->daughterboard_present[dgb_id] != RFNM_DAUGHTERBOARD_PRESENT ||
 		cfg->daughterboard_eeprom[dgb_id].board_id != RFNM_DAUGHTERBOARD_LIME) {
-		//printk("RFNM: Lime driver loaded, but daughterboard is not Lime\n");
 		memunmap(cfg);
-		// kernel_neon_end();
 		return -ENODEV;
 	}
 
@@ -659,16 +514,15 @@ static int rfnm_lime_probe(struct spi_device *spi)
 
 	rfnm_gpio_set(dgb_id, RFNM_DGB_GPIO3_22); // 1.4V LMS
 	rfnm_gpio_set(dgb_id, RFNM_DGB_GPIO4_8); // 1.25V LMS
-	
+
  	struct device *dev = &spi->dev;
 	struct rfnm_dgb *dgb_dt;
-	
+
 	const struct spi_device_id *id = spi_get_device_id(spi);
 	int i, ret;
 
 	dgb_dt = devm_kzalloc(dev, sizeof(struct rfnm_dgb), GFP_KERNEL);
 	if(!dgb_dt) {
-		// kernel_neon_end();
 		return -ENOMEM;
 	}
 
@@ -683,47 +537,36 @@ static int rfnm_lime_probe(struct spi_device *spi)
 
 	spi->max_speed_hz = 1000000;
 	spi->bits_per_word = 32;
-	//spi->mode = SPI_CPHA;
 	spi->mode = 0;
 
-
-	rfnm_fe_generic_init(dgb_dt, RFNM_LIME0_NUM_LATCHES); 
+	rfnm_fe_generic_init(dgb_dt, RFNM_LIME0_NUM_LATCHES);
 
 	lms7002m_hooks lms_hooks;
-	// lms_hooks = devm_kzalloc(dev, sizeof(lms7002m_hooks), GFP_KERNEL);
 
 	lms_hooks.log = rfnm_lime_log_callback;
 	lms_hooks.spi16_transact = lms_spi16_transact;
 	lms_hooks.spi16_userData = spi;
 	lms_hooks.on_cgen_frequency_changed = rfnm_lime_cgen_frequency_changed;
 
-	// LMS7002M_t *lms;
-    //LMS7_set_log_level(LMS7_DEBUG);
-    // lms = LMS7002M_create(lms_interface_transact, spi);
 	struct lms7002m_context *lms;
 	lms = lms7002m_create(&lms_hooks);
     if (lms == NULL) {
-		// kernel_neon_end();
         return -1;
     }
 	dgb_dt->priv_drv = lms;
-    // LMS7002M_reset(lms);
-	// lms7002m_soft_reset(lms);
-    // LMS7002M_set_spi_mode(lms, 4);
-	// lms_spi16_transact(0x8000 | 0x0021, )
 
 	const uint32_t reset_cmd = ((0x8000 | 0x0020) << 16) | 0x0000;
-	int spi_result = lms_spi16_transact(&reset_cmd, NULL, 1, spi);
+	int spi_result = spi_write(spi, &reset_cmd, 4);
 	const uint32_t release_reset_cmd = ((0x8000 | 0x0020) << 16) | 0xFFFD;
-	spi_result = lms_spi16_transact(&release_reset_cmd, NULL, 1, spi);
+	spi_result = spi_write(spi, &release_reset_cmd, 4);
 	// Both the new and old LimeSuite drivers do this on a reset. ??
 	const uint32_t write_mimo_ch_b_cmd = ((0x8000 | 0x002E) << 16) | 0x0000;
-	spi_result = lms_spi16_transact(&write_mimo_ch_b_cmd, NULL, 1, spi);
+	spi_result = spi_write(spi, &write_mimo_ch_b_cmd, 4);
 
 	// set 4-wire spi before reading back
 	// TODO: is this really necessary?
 	const uint32_t set_4wire_spi_cmd = ((0x8000 | 0x0021) << 16) | 0x0E9F;
-	spi_result = lms_spi16_transact(&set_4wire_spi_cmd, NULL, 1, spi);
+	spi_result = spi_write(spi, &set_4wire_spi_cmd, 4);
 
     //read info register
 	const uint32_t read_lms_info_reg_cmd = 0x002f << 16;
@@ -737,7 +580,6 @@ static int rfnm_lime_probe(struct spi_device *spi)
     if(!((read_lms_info_reg_value >> 6) & 0x1f)) {
         printk("LMS Not Found!");
 		lms7002m_destroy(lms);
-		// kernel_neon_end();
         return -1;
     }
 
@@ -755,33 +597,24 @@ static int rfnm_lime_probe(struct spi_device *spi)
 	lime_Result lms_ret = lms7002m_set_reference_clock(lms, LMS_REF_FREQ);
     if (ret != 0) {
         printk("provided reference clock invalid %d\n", ret);
-		// kernel_neon_end();
         return -1;
     }
 
-    // // rx path is always enabled
-    // LMS7002M_rbb_enable(lms, LMS_CHA, true);
-    // LMS7002M_sxx_enable(lms, LMS_RX, true);
-    // LMS7002M_rfe_enable(lms, LMS_CHA, true);
-
-	
-	// LMS7002M_sxx_enable(lms, LMS_TX, true);
-	// LMS7002M_tbb_enable(lms, LMS_CHA, true);
-	// LMS7002M_trf_enable(lms, LMS_CHA, true);
+    // rx path is always enabled
 	lms7002m_enable_channel(lms, false, LMS7002M_CHANNEL_A, true);
 
 	// TODO: do we need to do something like this still?
-	// // tune tx PLL to a high frequency out of the way to avoid spurs... meh. 
-	// LMS7002M_set_lo_freq(lms, LMS_TX, LMS_REF_FREQ, MHZ_TO_HZ(3800), &actualRate);
-	// // then disable some of it
-	// LMS7002M_sxx_enable(lms, LMS_TX, false);
+	// tune tx PLL to a high frequency out of the way to avoid spurs... meh.
+	lms7002m_set_frequency_sx(lms, true, MHZ_TO_HZ(3000));
+	// then disable some of it
+	lms7002m_enable_channel(lms, true, LMS7002M_CHANNEL_A, false);
 
 
 	//lime0_tx_power(dgb_dt, 1000, 100000);
 //	lime0_disable_all_pa(dgb_dt);
 /*
 	rfnm_fe_srb(dgb_dt, RFNM_LIME0_TL1O, 0);
-	
+
 
 
 	rfnm_fe_load_latches(dgb_dt);
@@ -798,22 +631,17 @@ static int rfnm_lime_probe(struct spi_device *spi)
 	rfnm_fe_load_latches(dgb_dt);
 	rfnm_fe_trigger_latches(dgb_dt);
 
-
 	printk("RFNM: Lime daughterboard initialized\n");
-
-	// kernel_neon_end();
-
 
 	struct rfnm_api_tx_ch *tx_ch, *tx_s;
 	struct rfnm_api_rx_ch *rx_ch, *rx_s;
 
 	tx_ch = devm_kzalloc(dev, sizeof(struct rfnm_api_tx_ch), GFP_KERNEL);
 	rx_ch = devm_kzalloc(dev, sizeof(struct rfnm_api_rx_ch), GFP_KERNEL);
-	tx_s = devm_kzalloc(dev, sizeof(struct rfnm_api_tx_ch), GFP_KERNEL);		
+	tx_s = devm_kzalloc(dev, sizeof(struct rfnm_api_tx_ch), GFP_KERNEL);
 	rx_s = devm_kzalloc(dev, sizeof(struct rfnm_api_rx_ch), GFP_KERNEL);
 
 	if(!tx_ch || !rx_ch || !tx_s || !rx_s) {
-		// kernel_neon_end();
 		return -ENOMEM;
 	}
 
@@ -841,28 +669,21 @@ static int rfnm_lime_probe(struct spi_device *spi)
 	dgb_dt->dac_ifs = 0x7;
 	dgb_dt->dac_iqswap[0] = 0;
 	dgb_dt->dac_iqswap[1] = 0;
-	//dgb_dt->adc_iqswap[0] = 0;
 	dgb_dt->adc_iqswap[0] = 1;
 	dgb_dt->adc_iqswap[1] = 0;
 	rfnm_dgb_reg(dgb_dt);
 
-	
 	return 0;
 }
 
 static int rfnm_lime_remove(struct spi_device *spi)
 {
-
 	struct rfnm_dgb *dgb_dt;
 	dgb_dt = spi_get_drvdata(spi);
 
 	lms7002m_destroy(dgb_dt->priv_drv);
 
-	//struct spi_controller *ctlr;
-
-	//ctlr = dev_get_drvdata(&pdev->dev);
-	//spi_unregister_controller(ctlr);
-	rfnm_dgb_unreg(dgb_dt); 
+	rfnm_dgb_unreg(dgb_dt);
 
 	rfnm_gpio_clear(dgb_dt->dgb_id, RFNM_DGB_GPIO3_22); // 1.4V LMS
 	rfnm_gpio_clear(dgb_dt->dgb_id, RFNM_DGB_GPIO4_8); // 1.25V LMS
